7c4ace0573de487be973432f480e9c93c6e7fde5
SunnySparks <Josediazt6@outlook.com>
2021-11-01 20:07:13 -0600

    Add book list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Category from './components/Categories';
 import Book from './components/Books';
 
 function App() {
   return (
-    <div>
-      <Book />
-      <Category />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Book</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Book />
+          </Route>
+          <Route path="/Categories">
+            <Category />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,50 +1,61 @@
 import '../App.css';
 import React from 'react';
 
-const Book = () => (
-  <div>
+const Book = () => {
+  const bookList = [
+    {
+      id: 1,
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: 2,
+      title: 'Harry Potter and the Philosopher\'s stone',
+      author: 'JK Rowling',
+    },
+    {
+      id: 3,
+      title: 'Ana Karenina',
+      author: 'León Tolstói',
+    },
+  ];
+  return (
     <div>
       <div>
-        <h1>
-          Your book list
-        </h1>
+        <div>
+          <h1>
+            Your book list
+          </h1>
+        </div>
       </div>
-    </div>
-    <div>
       <div>
-        <ul>
-          <li>
-            Book 1 by Author
-            <button type="button">Remove</button>
-          </li>
-          <li>
-            Book 2 by Author
-            <button type="button">Remove</button>
-          </li>
-          <li>
-            Book 3 by Author
-            <button type="button">Remove</button>
-          </li>
-          <li>
-            Book 4 by Author
-            <button type="button">Remove</button>
-          </li>
-          <li>
-            Book 5 by Author
-            <button type="button">Remove</button>
-          </li>
-        </ul>
+        <div>
+          <ul>
+            <div>
+              {bookList.map((book) => (
+                <li key={book.id}>
+                  <h4>
+                    {book.title}
+                    By:
+                    {book.author}
+                    <button type="button">Remove</button>
+                  </h4>
+                </li>
+              ))}
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <form action="/action_page.php">
+          <input type="text" name="title" value="Add your title" />
+          <br />
+          <input type="text" name="Author" value="Author's name" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     </div>
-    <div>
-      <form action="/action_page.php">
-        <input type="text" name="title" value="Add your title" />
-        <br />
-        <input type="text" name="Author" value="Author's name" />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Book;
