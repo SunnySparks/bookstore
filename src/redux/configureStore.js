67165178b3b208bd3c8dec8ef66fc127bc476660
SunnySparks <Josediazt6@outlook.com>
2021-11-03 23:05:09 -0600

    Get api url id

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
 
+const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/yozOG7uq64vvExbwooD9`;
 const reducer = combineReducers({
   book: booksReducer,
 });
